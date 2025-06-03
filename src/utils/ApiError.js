47c1cad9007f2ce6fc332d43d19cecaae8a3f1c2
soumya <soumayranjanswain609@gmail.com>
2025-06-03 12:05:38 +0530

    util code discussion

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+import { Error } from "mongoose";
+
+class ApiError extends Error {
+    constructor(
+        statuscode,
+        message= "something went wrong",
+        error = [],
+        statck = ""
+    ){
+        super(message)
+        this.statuscode = statuscode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (statck) {
+            this.stack = statck
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+export {ApiError}
